ac01fa8aec9df3c3d408bfcca208aa5b86a8c28f
Kasper Aamodt <44305520+kasperaamodt@users.noreply.github.com>
2021-05-13 16:21:16 +0200

    icon

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => 'd9a20b3bce1e840f4844d5088230e653');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '71da7eb86b6f9b50fe3220c7b714e263');
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { registerBlockType } from '@wordpress/blocks';
 import './style.scss';
 import Edit from './edit';
 import Save from './save';
-import { group as icon } from '@wordpress/icons';
+import icon from '../assets/section.svg';
 
 registerBlockType( 'agtech/agtech-section', {
 	icon,
